ac1dc61ed721c0a510e13ba3047c04414c16448e
divya dk <divya.dk@sap.com>
2025-01-03 09:50:03 +0000

    updated query

--- pages/Workflow/jsobjects/saveWorkflow/saveWorkflow.js
+++ pages/Workflow/jsobjects/saveWorkflow/saveWorkflow.js
@@ -28,25 +28,61 @@ export default {
 		// return generateUUID();
 		storeValue('UUID', getUUID());
 		if (dropdownValue && srcIp.text && destIp.text) {
+			// if (ipv4Regex.test(srcIp.text) && ipv4Regex.test(destIp.text)) {
+			// return Promise.all([
+			// saveWorkflowQuery.run(),
+			// executeWorkflowApi.run() 
+			// ])
+			// .then(() => showAlert('New workflow created successfully', 'Success'))
+			// .then(() => resetWidget('workflowForm'))
+			// .catch((error) => {
+			// // showAlert("Error: " + error.message);
+			// });
+			// } 
 			if (ipv4Regex.test(srcIp.text) && ipv4Regex.test(destIp.text)) {
-				return Promise.all([
-					saveWorkflowQuery.run(),
-					executeWorkflowApi.run() 
-				])
-					.then(() => showAlert('New workflow created successfully', 'Success'))
-					.then(() => resetWidget('workflowForm'))
+				saveWorkflowQuery.run()
+					.then(() => {
+					// Only call executeWorkflowApi after the first one succeeds
+					return executeWorkflowApi.run();
+				})
+					.then(() => {
+					// Both calls have succeeded, show success alert
+					showAlert('New workflow created successfully', 'Success');
+
+					// Reset the form
+					resetWidget('workflowForm');
+				})
 					.catch((error) => {
-					// showAlert("Error: " + error.message);
+					// showAlert("Error ipv4: " + error);
+					// Handle any errors that occur during the API calls
+					showAlert("Error ipv4: " + error.message);
 				});
-			} 
+			}
+
 			else if (ipv6Regex.test(srcIp.text) && ipv6Regex.test(destIp.text)) {
-				return Promise.all([
-					saveWorkflowQuery.run(),   // Run the PostgreSQL query
-					executeWorkflowApi.run() // Run the API call
-				])
-					.then(() => showAlert('New workflow created successfully', 'Success'))
-					.then(() => resetWidget('workflowForm'))
+				// return Promise.all([
+				// saveWorkflowQuery.run(),   // Run the PostgreSQL query
+				// executeWorkflowApi.run() // Run the API call
+				// ])
+				// .then(() => showAlert('New workflow created successfully', 'Success'))
+				// .then(() => resetWidget('workflowForm'))
+				// .catch((error) => {
+				// showAlert("Error: " + error.message);
+				// });
+				saveWorkflowQuery.run()
+					.then(() => {
+					// Only call executeWorkflowApi after the first one succeeds
+					return executeWorkflowApi.run();
+				})
+					.then(() => {
+					// Both calls have succeeded, show success alert
+					showAlert('New workflow created successfully', 'Success');
+
+					// Reset the form
+					resetWidget('workflowForm');
+				})
 					.catch((error) => {
+					// Handle any errors that occur during the API calls
 					showAlert("Error: " + error.message);
 				});
 			} else {
