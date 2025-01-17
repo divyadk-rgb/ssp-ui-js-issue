23a9273e8496a1552308c43a8456c5abfe62a9f5
divya dk <divya.dk@sap.com>
2025-01-17 11:15:47 +0000

    updated details page to enable detail container only for the workflow owner

--- pages/WorkflowDetails/jsobjects/storeRequestId/storeRequestId.js
+++ pages/WorkflowDetails/jsobjects/storeRequestId/storeRequestId.js
@@ -2,7 +2,19 @@ export default {
 	onLoad: () => {
 		const requestId = appsmith.URL.queryParams['id'];
 		storeValue('requestId', requestId);
-		// showAlert("Page has loaded!" + requestId);
+
+		const email = appsmith.URL.queryParams['email'];
+		storeValue('email', email);
+
+		// Check if the email in the query parameter matches the logged-in user's email
+		if (email === appsmith.user.email) {
+			// If the emails match, load data for the logged-in user (e.g., set a flag or load specific data)
+			storeValue('isUserMatched', true); 
+		} else {
+			// If the emails don't match, load a message or handle the condition
+			storeValue('isUserMatched', false);
+		}
+
 	}
 }
 
